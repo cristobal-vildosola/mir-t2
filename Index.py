053ae29c8adea2764c156a4f00a4f8bec880373b
Cristobal Muñoz <cristobal.vildosola@gmail.com>
2018-11-24 19:04:23 -0300

    crear clases para manejar índices

--- Index.py
+++ Index.py
@@ -1,12 +1,60 @@
+import time
+
 import pyflann
 
 
-class Index:
+class Linear:
 
-    def __init__(self, dataset_q, dataset_r, algorithm="linear", checks=-1):
+    def __init__(self, dataset):
         self.flann = pyflann.FLANN()
 
-        self.dataset_q = dataset_q
-        self.dataset_r = dataset_r
-        self.flann.build_index(dataset_r, algorithm=algorithm)
-        self.results, self.dists = self.flann.nn_index(dataset_q, num_neighbors=1, cores=1, checks=checks)
+        t0 = time.time()
+        self.flann.build_index(dataset, algorithm="linear")
+        t1 = time.time()
+
+        self.build_time = t1 - t0
+
+    def search(self, queryset):
+        t0 = time.time()
+        _, dists = self.flann.nn_index(queryset, num_neighbors=1, cores=1)
+        t1 = time.time()
+
+        return dists, t1 - t0
+
+
+class KDTree:
+
+    def __init__(self, dataset, trees):
+        self.flann = pyflann.FLANN()
+
+        t0 = time.time()
+        self.flann.build_index(dataset, algorithm="kdtree", trees=trees)
+        t1 = time.time()
+
+        self.build_time = t1 - t0
+
+    def search(self, queryset, checks):
+        t0 = time.time()
+        _, dists = self.flann.nn_index(queryset, checks=checks, num_neighbors=1, cores=1)
+        t1 = time.time()
+
+        return dists, t1 - t0
+
+
+class KmeansTree:
+
+    def __init__(self, dataset, branching):
+        self.flann = pyflann.FLANN()
+
+        t0 = time.time()
+        self.flann.build_index(dataset, algorithm="kmeans", branching=branching)
+        t1 = time.time()
+
+        self.build_time = t1 - t0
+
+    def search(self, queryset, checks):
+        t0 = time.time()
+        _, dists = self.flann.nn_index(queryset, checks=checks, num_neighbors=1, cores=1)
+        t1 = time.time()
+
+        return dists, t1 - t0
