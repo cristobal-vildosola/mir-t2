28f236f5aa299d8e0c21962b150a23ae18ffbb16
Cristobal Muñoz <cristobal.vildosola@gmail.com>
2018-11-26 23:57:20 -0300

    Arreglar gráfico y tabla en el notebook

--- Results.py
+++ Results.py
@@ -45,20 +45,65 @@ def obtener_curva(indice, queryset, lscan_time, lscan_dists):
     return efectividad, eficiencia
 
 
-def graficar_curvas(curvas, leyenda):
-    plt.figure()
+def graficar_curvas(curvas, leyenda, titulo):
+    plt.figure(figsize=(20, 6))
+
+    # curvas
+    plt.subplot(1, 2, 1)
+
+    # graficar curvas y obtener límite en y
+    max_efic = 1.0
+    for efectividad, eficiencia in curvas:
+        p = plt.plot(efectividad, eficiencia, 'o-')
+        max_efic = max(max_efic, max(eficiencia))
+
+    # configuración
     plt.xlabel('Efectividad')
     plt.ylabel('Eficiencia')
-    plt.grid(True)
-    max_efic = 1.0
-
-    for efectividad, eficiencia in curvas:
-        plt.plot(efectividad, eficiencia, 'o-')
-        max_efic = max(max_efic, max(eficiencia))
 
     plt.xlim([0, 100])
     plt.ylim([0, max_efic])
 
     plt.legend(leyenda)
+    plt.title(titulo)
+
+    # tabla
+    plt.subplot(1, 2, 2)
+
+    # obtener número de filas de la tabla
+    rows = 0
+    for efectividad, _ in curvas:
+        rows = max(rows, len(efectividad))
+
+    # generar texto de la tabla
+    cells_text = [[] for _ in range(rows)]
+    for efectividad, eficiencia in curvas:
+
+        # agregar datos de la columna
+        for i in range(len(efectividad)):
+            cells_text[i].append('%1.1f%% / %1.1fT' % (efectividad[i], eficiencia[i]))
+
+        # rellenar con vacío
+        for i in range(len(efectividad), rows):
+            cells_text[i].append('')
+
+    # esconder gráfico vacío
+    plt.axis('tight')
+    plt.axis('off')
+
+    # graficar tabla
+    colors = ["C%i" % i for i in range(len(leyenda))]
+    table = plt.table(cellText=cells_text,
+                      colLabels=leyenda,
+                      colColours=colors,
+                      rowLabels=["Punto %i" % i for i in range(rows)],
+                      loc='center left')
+
+    # agrandar tamaño letra
+    table.auto_set_font_size(False)
+    table.set_fontsize(14)
+    table.scale(1.2, 1.4)
+
+    print("hacer doble click sobre la imagen para ver en definición completa")
     plt.show()
     return
